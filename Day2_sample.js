2691040129c952e594fe0bc0c6515ce3cda37cc9
Dinesh kumar <dineshkumar@Dineshs-MacBook-Air.local>
2024-04-21 19:06:39 +0530

    added new

---
+++ Day2_sample.js
@@ -0,0 +1,20 @@
+//call by value example:
+
+function numd(value) {
+  value = 200;
+  console.log("value", value);
+}
+
+let b = 10;
+numd(b);
+
+function chnageobj(obj) {
+ console.log(obj);
+  obj.country="india";
+  console.log("obj",obj);
+
+}
+ let person={name:"santh",age:20};
+ chnageobj(person);
+ console.log("details",person);
+
