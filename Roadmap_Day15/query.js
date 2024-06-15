2ccde857015ff5e955ee5a1131a9d75c59db14b7
Dinesh kumar <dineshkumar@Dineshs-MacBook-Air.local>
2024-06-15 19:55:54 +0530

    the new class

---
+++ Roadmap_Day15/query.js
@@ -0,0 +1,23 @@
+//callback function
+
+// function a(b) {
+//     console.log("inside a ");
+//     b();
+    
+// }
+
+// a(()=>{
+//     console.log("inside be");
+// })
+// document.addEventListener("DOMContentLoaded",function(){
+//     let c=0
+//      const btn=document.getElementById('value');
+//      btn.addEventListener('click',()=>{
+//         console.log("the value",++c);
+//      })
+// });
+
+document.getElementById('value').addEventListener('click',()=>{
+    console.log("the value");
+})
+
